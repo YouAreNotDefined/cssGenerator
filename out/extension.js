60b1133782e5c8b831def2d40e78eb73bf9faacf
YouAreNotDefined <for.1self.ik@gmail.com>
2021-01-31 19:55:21 +0900

    fix

--- out/extension.js
+++ out/extension.js
@@ -14,7 +14,7 @@ function activate(context) {
         vscode.Uri.file(outDir),
         vscode.Uri.file(messageJsDir),
     ];
-    let disposable = vscode.commands.registerCommand('cssgenerator', () => {
+    let disposable = vscode.commands.registerCommand('CssGenerator', () => {
         const editor = vscode.window.activeTextEditor;
         if (typeof editor === 'undefined') {
             vscode.window.showInformationMessage('No active window!');
@@ -41,35 +41,41 @@ function activate(context) {
             localResourceRoots: resourceRoot,
         });
         viewPanel.webview.html = getHtml(html, inssertScript);
-        let cssObj;
-        let cssTexts = '';
+        const config = vscode.workspace.getConfiguration('CssGenerator');
+        const configMinWidth = config.get('minWidth');
+        const configMaxWidth = config.get('maxWidth');
+        const isNeededReset = config.get('resetCss');
+        const ignorantClass = config.get('ignorantClass');
+        assertIsDefined(configMinWidth);
+        assertIsDefined(configMaxWidth);
+        assertIsDefined(isNeededReset);
+        assertIsDefined(ignorantClass);
         viewPanel.webview.onDidReceiveMessage((data) => {
-            cssObj = createTemplate(data, true, true);
-            cssObj.forEach(css => {
-                cssTexts = `${css.pc}\n${css.sp}\n`;
-            });
-            // Open a file
+            const cssObj = createTemplate(configMaxWidth, configMinWidth, isNeededReset, ignorantClass, data);
+            const cssTexts = cssObj.reduce((acc, css, i) => {
+                if (i === cssObj.length - 1) {
+                    return `${acc}${css.pc}\n}${css.sp}\n}`;
+                }
+                else {
+                    return acc + css.pc + css.sp;
+                }
+            }, '');
             const uri = editor.document.uri.toString(true);
             const lastSlash = uri.lastIndexOf('/');
             const filePath = lastSlash >= 0 ? uri.substring(0, lastSlash) : '';
             const appPathIndex = vscode.env.appRoot.lastIndexOf('resources');
             const appPath = appPathIndex >= 0 ? vscode.env.appRoot.substring(0, appPathIndex) : '';
             // writeFile(vscode.Uri.parse(`${filePath}/style.css`), html, (err) => {
-            fs_1.writeFile('./style.css', cssTexts, (err) => {
+            fs_1.writeFile('./generated.css', cssTexts, (err) => {
                 if (err) {
                     vscode.window.showInformationMessage(`${err}`);
-                    // vscode.window.showInformationMessage(`${appPath}`);
                 }
                 else {
-                    // vscode.window.showInformationMessage(`${appPath}style.css`);
-                    // vscode.window.showInformationMessage('Success!');
-                    // vscode.window.showInformationMessage(`${cssTexts}`)
-                    vscode.workspace.openTextDocument(`${appPath}style.css`)
+                    vscode.window.showInformationMessage('Success!');
+                    vscode.workspace.openTextDocument(`${appPath}generated.css`)
                         .then(doc => {
                         vscode.window.showTextDocument(doc);
-                        setTimeout(() => {
-                            viewPanel.dispose();
-                        }, 3000);
+                        viewPanel.dispose();
                     });
                 }
             });
@@ -85,86 +91,95 @@ function getHtml(html, script) {
     const scriptTag = `<script type="text/javascript" src="${script}"></script>`;
     return `${bodyBeforeHtml}${scriptTag}${bodyAfterHtml}`;
 }
-function createTemplate(data, comment, needReset) {
-    const pcQuery = '@media screen and (min-width:768px) { \n';
-    const spQuery = '@media screen and (max-width:767px) { \n';
+function createTemplate(maxWidth, minWidth, needReset, ignorantClass, data) {
+    const pcQuery = `@media screen and (min-width:${minWidth}) { \n`;
+    const spQuery = `@media screen and (max-width:${maxWidth}) { \n`;
     const pc = `/*===== PC =====*/\n${pcQuery}`;
     const sp = `/*===== SP =====*/\n${spQuery}`;
-    let reset = '';
-    let charSet = '';
-    let resetComment = '';
-    let resetCss = '';
-    if (needReset) {
-        charSet = '@charset "UTF-8";\n';
-        resetComment = '/*==============================\nリセットCSS\n==============================*/\n/*===== PC/Tablet/SP =====*/\n';
-        resetCss = 'html, body, div, span, applet, object, iframe,\nh1, h2, h3, h4, h5, h6, p, blockquote, pre,\na, abbr, acronym, address, \nbig, cite, code,\ndel, dfn, em, img, ins, kbd, q, s, samp,\nsmall, strike, strong, sub, sup, tt, var,\nb, u, i, dl, dt, dd, ol, ul, li,\nfieldset, form, label, legend,\ntable, caption, tbody, tfoot, thead, tr, th, td,\narticle, aside, canvas, details, embed,\nfigure, figcaption, footer, header, hgroup,\nmenu, nav, output, ruby, section, summary,\ntime, mark, audio, video {\nmargin: 0;\npadding: 0;\nborder: 0;\nfont-size: 100%;\nfont: inherit;\nvertical-align: baseline;\n}\n* {\nbox-sizing: border-box;\noutline: none;\n}\narticle, aside, details, figcaption, figure,\nfooter, header, hgroup, menu, nav, section {\ndisplay: block;\n}\nbody, html {\ndisplay: block;\n}\nbody {\n-webkit-text-size-adjust: 100 %;\n}\nimg {\nmax-width: 100 %;\nheight: auto;\nvertical-align: bottom;\n}\nol, ul {\nlist-style: none;\n}\ntable {\nborder-collapse: collapse;\nborder-spacing: 0;\n}\nbtn {\nmargin: 0;\npadding: 0;\nbackground: none;\nborder: 0;\ncursor: pointer;\n}\n\n';
-        reset = `${charSet}${resetComment}${resetCss}`;
+    let unnecessaryClass = [];
+    let allData = data;
+    if (ignorantClass !== '') {
+        unnecessaryClass = ignorantClass.split(',');
+        allData = data.filter(className => !unnecessaryClass.includes(className));
     }
-    let untillUnderStr = [];
-    const sameStr1 = data
-        .map((el, i) => {
-        const underIndex = el.indexOf('__');
-        const storeStr = underIndex >= 0 ? el.slice(0, underIndex) : '';
-        const hasNotStr = untillUnderStr.every(str => str !== storeStr);
-        untillUnderStr[i] = storeStr;
-        if (hasNotStr && underIndex >= 0) {
-            return untillUnderStr[i];
-        }
-    })
-        .filter((str) => str !== undefined);
-    let beforeLineStr = [];
-    const sameStr2 = data
-        .map((el, i) => {
-        const containStr2 = sameStr1.find(str1 => el.indexOf(`${str1}__`));
-        if (containStr2) {
-            const reg = new RegExp(`${containStr2}__`);
-            const str2AfterStr1 = el.replace(reg, '');
-            const lineIndex = str2AfterStr1.search(/-/);
-            const storeStr = lineIndex >= 0 ? str2AfterStr1.slice(0, lineIndex) : str2AfterStr1;
-            const hasNotStr = beforeLineStr.every(str => str !== storeStr);
-            beforeLineStr[i] = storeStr;
-            if (hasNotStr) {
-                return storeStr;
+    ;
+    const charSet = '@charset "UTF-8";\n';
+    let strs = [];
+    const pattern = /[-,_]+/;
+    const sameStr1 = allData
+        .map(el => {
+        const isContainPattern = pattern.test(el);
+        const strArray = isContainPattern ? el.split(pattern, 2) : el;
+        if (strArray instanceof Array) {
+            const hasNotStr = strs.every(str => str.join() !== strArray.join());
+            strs.push(strArray);
+            if (hasNotStr && isContainPattern) {
+                return strArray;
             }
         }
     })
         .filter((str) => str !== undefined);
-    vscode.window.showInformationMessage(`${sameStr2}`);
     let css = [{ pc: '', sp: '' }];
-    sameStr1.forEach((str1, i) => {
-        let isFirst1 = true;
-        const commentText1 = getComment(str1, true);
-        sameStr2.forEach((str2, j) => {
-            let isFirst2 = true;
-            const commentText2 = getComment(str2, false);
-            data.forEach(className => {
-                if (className.indexOf(str1) || className.indexOf(str2)) {
-                    if (className.indexOf(str1)) {
-                        if (isFirst1) {
-                            css[i].pc += `\n${commentText1}${pc}`;
-                            css[i].sp += `\n${commentText1}${sp}`;
-                            isFirst1 = false;
-                        }
-                    }
-                    if (className.indexOf(str2)) {
-                        if (isFirst2) {
-                            css[i].pc += `\n${commentText2}\n`;
-                            css[i].sp += `\n${commentText2}\n`;
-                            isFirst2 = false;
-                        }
-                    }
-                    css[i].pc += `.${className}{\n\n}\n`;
-                    css[i].sp += `.${className}{\n\n}\n`;
-                }
-                if (sameStr2.length - 1 === j) {
-                    css[i].pc += '}\n';
-                    css[i].sp += '}\n';
-                }
+    css[0].pc += charSet;
+    if (needReset) {
+        const resetComment = '\n/*==============================\nリセットCSS\n==============================*/\n/*===== PC/Tablet/SP =====*/\n';
+        const resetCss = 'html, body, div, span, applet, object, iframe,\nh1, h2, h3, h4, h5, h6, p, blockquote, pre,\na, abbr, acronym, address, \nbig, cite, code,\ndel, dfn, em, img, ins, kbd, q, s, samp,\nsmall, strike, strong, sub, sup, tt, var,\nb, u, i, dl, dt, dd, ol, ul, li,\nfieldset, form, label, legend,\ntable, caption, tbody, tfoot, thead, tr, th, td,\narticle, aside, canvas, details, embed,\nfigure, figcaption, footer, header, hgroup,\nmenu, nav, output, ruby, section, summary,\ntime, mark, audio, video {\n\tmargin: 0;\n\tpadding: 0;\n\tborder: 0;\n\tfont-size: 100%;\n\tfont: inherit;\n\tvertical-align: baseline;\n}\n* {\n\tbox-sizing: border-box;\n\toutline: none;\n}\narticle, aside, details, figcaption, figure,\nfooter, header, hgroup, menu, nav, section {\n\tdisplay: block;\n}\nbody, html {\n\tdisplay: block;\n}\nbody {\n\t-webkit-text-size-adjust: 100%;\n}\nimg {\n\tmax-width: 100%;\n\theight: auto;\n\tvertical-align: bottom;\n}\nol, ul {\n\tlist-style: none;\n}\ntable {\n\tborder-collapse: collapse;\n\tborder-spacing: 0;\n}\nbtn {\n\tmargin: 0;\n\tpadding: 0;\n\tbackground: none;\n\tborder: 0;\n\tcursor: pointer;\n}\n\n';
+        css[0].pc += resetComment + resetCss;
+    }
+    let isFirst1 = true;
+    let beforeStr1 = [];
+    let i = 0;
+    let NoPatternStore;
+    allData.forEach(className => {
+        var _a;
+        const classStr1 = (_a = sameStr1.find(str1 => {
+            const reg = new RegExp(`${str1[0]}[-,_]+${str1[1]}`);
+            return className.search(reg) >= 0;
+        })) === null || _a === void 0 ? void 0 : _a.join(' ');
+        const NoPatternClass = sameStr1.find(str1 => str1[0] === className);
+        if (classStr1) {
+            const isNotAdded1 = beforeStr1.every(str1 => {
+                const reg1 = new RegExp(str1);
+                return classStr1.search(reg1) === -1;
             });
-        });
+            if (isNotAdded1 && beforeStr1.length > 0) {
+                isFirst1 = true;
+                css[i].pc += '}\n';
+                css[i].sp += '}\n';
+                i += 1;
+                css.push({ pc: '', sp: '' });
+            }
+            beforeStr1.push(classStr1);
+            if (isFirst1) {
+                const commentText1 = getComment(classStr1, true);
+                css[i].pc += `\n${commentText1}\n${pc}`;
+                css[i].sp += `\n${sp}`;
+                if (NoPatternStore) {
+                    css[i].pc += `\t.${NoPatternStore}{\n\n\t}\n`;
+                    css[i].sp += `\t.${NoPatternStore}{\n\n\t}\n`;
+                }
+                NoPatternStore = false;
+                isFirst1 = false;
+            }
+        }
+        if (!NoPatternClass && className !== ignorantClass) {
+            const regClass = new RegExp(className);
+            const isNotAddedClass = css.every(cssText => !regClass.test(cssText.pc));
+            if (isNotAddedClass) {
+                css[i].pc += `\t.${className}{\n\n\t}\n`;
+                css[i].sp += `\t.${className}{\n\n\t}\n`;
+            }
+        }
+        else {
+            NoPatternStore = className;
+        }
     });
-    vscode.window.showInformationMessage(`${css}`);
     return css;
+}
+function assertIsDefined(val) {
+    if (val === undefined || val === null) {
+        throw new Error(`Expected 'val' to be defined, but received ${val}`);
+    }
 }
 function getComment(text, isTitle) {
     const topCommentOut = '/*====================';
--- src/extension.ts
+++ src/extension.ts
@@ -18,7 +18,7 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.Uri.file(messageJsDir),
 	];
 
-	let disposable = vscode.commands.registerCommand('cssgenerator', () => {
+	let disposable = vscode.commands.registerCommand('CssGenerator', () => {
 		const editor = vscode.window.activeTextEditor;
 		if (typeof editor === 'undefined') {
 			vscode.window.showInformationMessage('No active window!');
@@ -51,42 +51,46 @@ export function activate(context: vscode.ExtensionContext) {
 			})
 		viewPanel.webview.html = getHtml(html, inssertScript);
 
-		let cssObj:css[];
-		let cssTexts = '';
-		viewPanel.webview.onDidReceiveMessage((data: string[]) => {
-			cssObj = createTemplate(data, true, true);
-			cssObj.forEach(css => {
-				cssTexts = `${css.pc}\n${css.sp}\n`;
-			})
+		const config = vscode.workspace.getConfiguration('CssGenerator');
+		const configMinWidth: number | undefined = config.get('minWidth');
+		const configMaxWidth: number | undefined = config.get('maxWidth');
+		const isNeededReset: boolean | undefined = config.get('resetCss');
+		const ignorantClass: string | undefined = config.get('ignorantClass');
+		assertIsDefined(configMinWidth);
+		assertIsDefined(configMaxWidth);
+		assertIsDefined(isNeededReset);
+		assertIsDefined(ignorantClass);
 
-			// Open a file
+		viewPanel.webview.onDidReceiveMessage((data: string[]) => {
+			const cssObj = createTemplate(configMaxWidth,configMinWidth,isNeededReset,ignorantClass,data);
+			const cssTexts = cssObj.reduce((acc, css, i) => {
+				if (i === cssObj.length - 1) {
+					return `${acc}${css.pc}\n}${css.sp}\n}`;
+				} else {
+					return acc + css.pc + css.sp;
+				}
+			}, '');
+
 			const uri = editor.document.uri.toString(true);
 			const lastSlash = uri.lastIndexOf('/');
 			const filePath = lastSlash >= 0 ? uri.substring(0, lastSlash) : '';
 			const appPathIndex = vscode.env.appRoot.lastIndexOf('resources');
 			const appPath = appPathIndex >= 0 ? vscode.env.appRoot.substring(0,appPathIndex) : '';
 			// writeFile(vscode.Uri.parse(`${filePath}/style.css`), html, (err) => {
-			writeFile('./style.css', cssTexts, (err) => {
+			writeFile('./generated.css', cssTexts, (err) => {
 				if (err) {
 					vscode.window.showInformationMessage(`${err}`);
-					// vscode.window.showInformationMessage(`${appPath}`);
 				} else {
-					// vscode.window.showInformationMessage(`${appPath}style.css`);
-					// vscode.window.showInformationMessage('Success!');
-					// vscode.window.showInformationMessage(`${cssTexts}`)
-					vscode.workspace.openTextDocument(`${appPath}style.css`)
+					vscode.window.showInformationMessage('Success!');
+					vscode.workspace.openTextDocument(`${appPath}generated.css`)
 						.then(doc => {
 							vscode.window.showTextDocument(doc);
-							setTimeout(() => {
-								viewPanel.dispose();
-							}, 3000);
-						})
+							viewPanel.dispose();
+						});
 				}
 			});
 		})
-
 	});
-
 	context.subscriptions.push(disposable);
 }
 
@@ -98,95 +102,106 @@ function getHtml(html:string, script:string) {
 	return `${bodyBeforeHtml}${scriptTag}${bodyAfterHtml}`;
 }
 
-function createTemplate(data:string[],comment:boolean,needReset:boolean) {
-	const pcQuery = '@media screen and (min-width:768px) { \n';
-	const spQuery = '@media screen and (max-width:767px) { \n';
+function createTemplate(maxWidth: number, minWidth: number ,needReset: boolean,ignorantClass: string, data: string[]) {
+	const pcQuery = `@media screen and (min-width:${minWidth}) { \n`;
+	const spQuery = `@media screen and (max-width:${maxWidth}) { \n`;
 	const pc = `/*===== PC =====*/\n${pcQuery}`;
 	const sp = `/*===== SP =====*/\n${spQuery}`;
+	let unnecessaryClass: string[] = [];
+	let allData = data;
+	if (ignorantClass !== '') {
+		unnecessaryClass = ignorantClass.split(',');
+		allData = data.filter(className => !unnecessaryClass.includes(className));
+	};
 
-	let reset = '';
-	let charSet = '';
-	let resetComment = '';
-	let resetCss = '';
+	const charSet = '@charset "UTF-8";\n';
 
-	if (needReset) {
-		charSet = '@charset "UTF-8";\n'
-		resetComment = '/*==============================\nリセットCSS\n==============================*/\n/*===== PC/Tablet/SP =====*/\n';
-		resetCss = 'html, body, div, span, applet, object, iframe,\nh1, h2, h3, h4, h5, h6, p, blockquote, pre,\na, abbr, acronym, address, \nbig, cite, code,\ndel, dfn, em, img, ins, kbd, q, s, samp,\nsmall, strike, strong, sub, sup, tt, var,\nb, u, i, dl, dt, dd, ol, ul, li,\nfieldset, form, label, legend,\ntable, caption, tbody, tfoot, thead, tr, th, td,\narticle, aside, canvas, details, embed,\nfigure, figcaption, footer, header, hgroup,\nmenu, nav, output, ruby, section, summary,\ntime, mark, audio, video {\nmargin: 0;\npadding: 0;\nborder: 0;\nfont-size: 100%;\nfont: inherit;\nvertical-align: baseline;\n}\n* {\nbox-sizing: border-box;\noutline: none;\n}\narticle, aside, details, figcaption, figure,\nfooter, header, hgroup, menu, nav, section {\ndisplay: block;\n}\nbody, html {\ndisplay: block;\n}\nbody {\n-webkit-text-size-adjust: 100 %;\n}\nimg {\nmax-width: 100 %;\nheight: auto;\nvertical-align: bottom;\n}\nol, ul {\nlist-style: none;\n}\ntable {\nborder-collapse: collapse;\nborder-spacing: 0;\n}\nbtn {\nmargin: 0;\npadding: 0;\nbackground: none;\nborder: 0;\ncursor: pointer;\n}\n\n';
-		reset = `${charSet}${resetComment}${resetCss}`;
-	}
+	let strs: string[][] = [];
+	const pattern = /[-,_]+/;
 
-	let untillUnderStr : string[] = [];
-
-	const sameStr1 = data
-	.map((el,i) => {
-		const underIndex = el.indexOf('__');
-		const storeStr = underIndex >= 0 ? el.slice(0, underIndex) : '';
-		const hasNotStr = untillUnderStr.every(str => str !== storeStr);
-		untillUnderStr[i] = storeStr;
-		if (hasNotStr && underIndex >= 0) {
-			return untillUnderStr[i];
-		}
-	})
-	.filter((str) : str is Exclude<typeof str, undefined> => str !== undefined);
-
-	let beforeLineStr: string[] = [];
-
-	const sameStr2 = data
-		.map((el, i) => {
-			const containStr2 = sameStr1.find(str1 => el.indexOf(`${str1}__`));
-			if (containStr2) {
-				const reg = new RegExp(`${containStr2}__`);
-				const str2AfterStr1 = el.replace(reg, '');
-				const lineIndex = str2AfterStr1.search(/-/);
-				const storeStr = lineIndex >= 0 ? str2AfterStr1.slice(0, lineIndex) : str2AfterStr1;
-				const hasNotStr = beforeLineStr.every(str => str !== storeStr);
-				beforeLineStr[i] = storeStr;
-				if (hasNotStr) {
-					return storeStr;
+	const sameStr1 = allData
+		.map(el => {
+			const isContainPattern = pattern.test(el);
+			const strArray = isContainPattern ? el.split(pattern, 2) : el;
+			if (strArray instanceof Array) {
+				const hasNotStr = strs.every(str => str.join() !== strArray.join());
+				strs.push(strArray);
+				if (hasNotStr && isContainPattern) {
+					return strArray;
 				}
 			}
 		})
 		.filter((str): str is Exclude<typeof str, undefined> => str !== undefined);
-	vscode.window.showInformationMessage(`${sameStr2}`)
 
-	let css:css[] = [{ pc: '', sp: '' }];
+	let css: css[] = [{ pc: '', sp: '' }];
+	css[0].pc += charSet;
 
-	sameStr1.forEach((str1,i) => {
-		let isFirst1 = true;
-		const commentText1 = getComment(str1, true);
-		sameStr2.forEach((str2,j) => {
-			let isFirst2 = true;
-			const commentText2 = getComment(str2, false);
-			data.forEach(className => {
-				if (className.indexOf(str1) || className.indexOf(str2)) {
-					if (className.indexOf(str1)) {
-						if (isFirst1) {
-							css[i].pc += `\n${commentText1}${pc}`;
-							css[i].sp += `\n${commentText1}${sp}`;
-							isFirst1 = false;
-						}
-					}
-					if (className.indexOf(str2)) {
-						if (isFirst2) {
-							css[i].pc += `\n${commentText2}\n`;
-							css[i].sp += `\n${commentText2}\n`;
-							isFirst2 = false;
-						}
-					}
-					css[i].pc += `.${className}{\n\n}\n`;
-					css[i].sp += `.${className}{\n\n}\n`;
+	if (needReset) {
+		const resetComment = '\n/*==============================\nリセットCSS\n==============================*/\n/*===== PC/Tablet/SP =====*/\n';
+		const resetCss = 'html, body, div, span, applet, object, iframe,\nh1, h2, h3, h4, h5, h6, p, blockquote, pre,\na, abbr, acronym, address, \nbig, cite, code,\ndel, dfn, em, img, ins, kbd, q, s, samp,\nsmall, strike, strong, sub, sup, tt, var,\nb, u, i, dl, dt, dd, ol, ul, li,\nfieldset, form, label, legend,\ntable, caption, tbody, tfoot, thead, tr, th, td,\narticle, aside, canvas, details, embed,\nfigure, figcaption, footer, header, hgroup,\nmenu, nav, output, ruby, section, summary,\ntime, mark, audio, video {\n\tmargin: 0;\n\tpadding: 0;\n\tborder: 0;\n\tfont-size: 100%;\n\tfont: inherit;\n\tvertical-align: baseline;\n}\n* {\n\tbox-sizing: border-box;\n\toutline: none;\n}\narticle, aside, details, figcaption, figure,\nfooter, header, hgroup, menu, nav, section {\n\tdisplay: block;\n}\nbody, html {\n\tdisplay: block;\n}\nbody {\n\t-webkit-text-size-adjust: 100%;\n}\nimg {\n\tmax-width: 100%;\n\theight: auto;\n\tvertical-align: bottom;\n}\nol, ul {\n\tlist-style: none;\n}\ntable {\n\tborder-collapse: collapse;\n\tborder-spacing: 0;\n}\nbtn {\n\tmargin: 0;\n\tpadding: 0;\n\tbackground: none;\n\tborder: 0;\n\tcursor: pointer;\n}\n\n';
+		css[0].pc += resetComment + resetCss;
+	}
+
+	let isFirst1 = true;
+	let beforeStr1: string[] = [];
+	let i = 0;
+	let NoPatternStore: string | boolean;
+
+	allData.forEach(className => {
+		const classStr1 = sameStr1.find(str1 => {
+			const reg = new RegExp(`${str1[0]}[-,_]+${str1[1]}`);
+			return className.search(reg) >= 0;
+		})?.join(' ');
+		const NoPatternClass = sameStr1.find(str1 => str1[0] === className);
+
+		if (classStr1) {
+			const isNotAdded1 = beforeStr1.every(str1 => {
+				const reg1 = new RegExp(str1);
+				return classStr1.search(reg1) === -1;
+			});
+
+			if (isNotAdded1 && beforeStr1.length > 0) {
+				isFirst1 = true;
+				css[i].pc += '}\n';
+				css[i].sp += '}\n';
+				i += 1;
+				css.push({ pc: '', sp: '' });
+			}
+			beforeStr1.push(classStr1);
+
+			if (isFirst1) {
+				const commentText1 = getComment(classStr1, true);
+				css[i].pc += `\n${commentText1}\n${pc}`;
+				css[i].sp += `\n${sp}`;
+				if (NoPatternStore) {
+					css[i].pc += `\t.${NoPatternStore}{\n\n\t}\n`;
+					css[i].sp += `\t.${NoPatternStore}{\n\n\t}\n`;
 				}
-				if (sameStr2.length - 1 === j) {
-					css[i].pc += '}\n';
-					css[i].sp += '}\n';
-				}
-			})
-		});
-	})
-	vscode.window.showInformationMessage(`${css}`)
+				NoPatternStore = false;
+				isFirst1 = false;
+			}
+		}
 
+		if (!NoPatternClass && className !== ignorantClass) {
+			const regClass = new RegExp(className);
+			const isNotAddedClass = css.every(cssText => !regClass.test(cssText.pc));
+			if (isNotAddedClass) {
+				css[i].pc += `\t.${className}{\n\n\t}\n`;
+				css[i].sp += `\t.${className}{\n\n\t}\n`;
+			}
+		} else {
+			NoPatternStore = className;
+		}
+	});
 	return css;
+}
+
+function assertIsDefined<T>(val: T): asserts val is NonNullable<T> {
+	if (val === undefined || val === null) {
+		throw new Error(
+			`Expected 'val' to be defined, but received ${val}`
+		);
+	}
 }
 
 function getComment(text: string, isTitle: boolean) {
